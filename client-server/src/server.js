c8fbf5499153bc14ccdd77941ed1b7c373860fb4
Parminder Singh <parminder3306@gmail.com>
2024-12-11 21:40:48 +0000

    modified

--- client-server/src/server.js
+++ client-server/src/server.js
@@ -11,6 +11,9 @@ const app = express();
 // app.use(cors());
 app.use(express.json());
 app.use("/api/v1/auth", authRoutes);
+// app.use("/api/v1/users", userRoutes);
+// app.use("/api/v1/transactions", transactionRoutes);
+// app.use("/api/v1/currency-rates", currencyRoutes);
 
 const connectDB = async () => {
   try {
