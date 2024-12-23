408dc87cba011bae7371f297874d3e3e1e0704cd
Parminder Singh <parminder3306@gmail.com>
2024-12-23 21:54:04 +0000

    modified

--- client-server/src/routes/Api.js
+++ client-server/src/routes/Api.js
@@ -3,7 +3,7 @@ import { Login, SignUp } from "../controllers/Authentication.js";
 
 const api = Router();
 
-api.post("auth/login", Login);
-api.post("auth/signup", SignUp);
+api.post("/auth/login", Login);
+api.post("/auth/signup", SignUp);
 
 export default api;
--- client-server/src/server.js
+++ client-server/src/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 
 import env from "./config/Env.js";
-import db from "./config/Database.js";
+import DB from "./config/Database.js";
 
 import api from "./routes/Api.js";
 
@@ -10,11 +10,11 @@ const app = express();
 
 // app.use(cors());
 app.use(express.json());
-app.use("/api/v1/", api);
+app.use("/api/v1", api);
 
 const connectDB = async () => {
   try {
-    await db.raw("SELECT 1+1 AS result");
+    await DB.raw("SELECT 1+1 AS result");
     console.log("🚀 Database connected successfully");
   } catch (error) {
     console.error("❌ Database connection failed:", error.message);
